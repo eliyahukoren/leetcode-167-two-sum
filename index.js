d7dbad91ead6e62dee7ef602de92070f6ce88252
eliyahukoren <elik.koren@gmail.com>
2022-12-27 12:33:38 +0200

    solution and tests

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-/* Happy hacking !*/
+module.exports = twoSum = function (nums, target) {
+	let start = 0;
+	let length = nums.length;
+	const res = [];
+	const exc = {};
+
+	while (start < length - 1) {
+		if (exc[nums[start]] !== undefined) {
+			start++;
+			continue;
+		}
+
+		for (let i = start + 1; i < length; i++) {
+			if (nums[start] + nums[i] == target) {
+				res.push(...res, start + 1, i + 1);
+			}
+
+			exc[nums[start]] = nums[start];
+		}
+		start++;
+	}
+
+	return res;
+};
--- test/index.test.js
+++ test/index.test.js
@@ -1,1 +1,10 @@
-const assert = require('assert');
+const assert = require("assert");
+const twoSum = require("../index");
+
+describe("Main tests", () => {
+	it("Basic tests", () => {
+		assert.deepEqual(twoSum([2, 7, 11, 15], 9), [1, 2]);
+		assert.deepEqual(twoSum([2, 3, 4], 6), [1, 3]);
+		assert.deepEqual(twoSum([-1, 0], -1), [1, 2]);
+	})
+})
